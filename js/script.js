3f32ba2da55ec36193bd913babc008e1064985d4
Prico3 <pricoco.andrea@gmail.com>
2022-10-06 15:38:36 +0200

    java completed

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+//Chiedere all'utente il numero di km che desidera percorrere
+const km = parseInt(prompt("Quanti km devi percorrere?"));
+
+//Chiedere all'utente quanti anni ha
+const age = parseInt(prompt("Quanti anni hai?"));
+console.log(km, age);
+
+//Calcoare il prezzo del biglietto 
+const initialTicketPrice = km * 0.21;
+console.log(initialTicketPrice);
+
+//Se l'utente ha meno di 18 anni applicare lo sconto del 20% E se l'utente ha più di 65 anni applicare uno sconto del 40%
+let ticketPrice = "";
+if (age <= 17) {
+    ticketPrice = initialTicketPrice * 0.8;
+} else if (age => 65){
+    ticketPrice = initialTicketPrice * 0.6; 
+}
+console.log(ticketPrice);
+
+//Prezzo finale
+document.getElementById("finalPrice").innerHTML = ticketPrice;
